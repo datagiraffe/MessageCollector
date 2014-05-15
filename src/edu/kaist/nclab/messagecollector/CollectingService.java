918648d7eeec39f1cc472546dd356214e005c514
Friedhelm <f.victor@kaist.ac.kr>
2014-05-15 14:01:33 +0200

    add getForegroundApplicationName method

--- src/edu/kaist/nclab/messagecollector/CollectingService.java
+++ src/edu/kaist/nclab/messagecollector/CollectingService.java
@@ -1,29 +1,88 @@
 package edu.kaist.nclab.messagecollector;
+
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningTaskInfo;
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.Toast;
 
-
 public class CollectingService extends AccessibilityService {
-  
+
+//	public static boolean isRunningProcess(Context context, String[] packageName) {
+//
+//		boolean isRunning = false;
+//		ActivityManager actMng = (ActivityManager) context
+//				.getSystemService(Context.ACTIVITY_SERVICE);
+//		List<RunningTaskInfo> list = actMng.getRunningTasks(1);
+//		for (RunningTaskInfo rap : list) {
+//			// Log.e(rap.baseActivity.getPackageName().toString());
+//			for (int i = 0; i < packageName.length; i++) {
+//				// Log.e("PACKAGE NAME : " + packageName[i]);
+//				if (rap.baseActivity.getPackageName().toString()
+//						.contains(packageName[i])) {
+//					isRunning = true;
+//					break;
+//				}
+//			}
+//
+//		}
+//		return isRunning;
+//	}
+	
+
+	//ActivityManager actMng = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
 	@Override
 	public void onCreate() {
 		Log.v(TAG, "onCreate");
-		Toast.makeText(getApplicationContext(), "Anne : bis spaeter", Toast.LENGTH_LONG).show();
+		Toast.makeText(getApplicationContext(), "Anne : bis spaeter",
+				Toast.LENGTH_LONG).show();
 		super.onCreate();
+
+	}
+
+	/**
+	 * Returns the name of the application that is currently in the foreground.
+	 * If it is not possible, if that Applications Package name cannot be found,
+	 * this method returns the String "unknown"
+	 * 
+	 * @return the name of the foreground application
+	 */
+	private String getForegroundApplicationName() {
+		ActivityManager actMng = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
+		// The first in the list of RunningTasks is always the foreground task.
+		RunningTaskInfo foregroundTaskInfo = actMng.getRunningTasks(1).get(0);
 		
+		// http://stackoverflow.com/questions/2166961/determining-the-current-foreground-application-from-a-background-task-or-service
+		String foregroundTaskPackageName = foregroundTaskInfo .topActivity.getPackageName();
+		PackageManager pm = this.getPackageManager();
+		PackageInfo foregroundAppPackageInfo;
+		String foregroundTaskAppName;
+		try {
+			foregroundAppPackageInfo = pm.getPackageInfo(foregroundTaskPackageName, 0);
+			foregroundTaskAppName = foregroundAppPackageInfo.applicationInfo.loadLabel(pm).toString();
+		} catch (NameNotFoundException e) {
+			foregroundTaskAppName = "unknown";
+		}
+		
+		return foregroundTaskAppName;
 	}
 
 	public static final String TAG = "CollectingService";
-	
+
 	@Override
 	public void onAccessibilityEvent(AccessibilityEvent event) {
-		Log.v(TAG, "***** onAccessibilityEvent with Text "+ event.getText() + " package: "+event.getPackageName());
+		Log.v(TAG, "application name: "+getForegroundApplicationName());
+		Log.v(TAG, "***** onAccessibilityEvent with Text " + event.getText()
+				+ " package: " + event.getPackageName());
 		CharSequence charPackageName = event.getPackageName();
 		String eventPackageName;
-		
+
 		if (charPackageName == null) {
 			eventPackageName = "ep";
 		} else {
@@ -31,36 +90,35 @@ public class CollectingService extends AccessibilityService {
 		}
 		// IF it is a Kakao Talk Message Event of length >= 3
 		if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED
-			&& event.getText().toString().length() >= 3
-			&& eventPackageName.contentEquals("com.kakao.talk")) {
-			
+				&& event.getText().toString().length() >= 3
+				&& eventPackageName.contentEquals("com.kakao.talk")) {
+
 			// Then collect the following data
 			long eventTime = event.getEventTime();
 			String eventText = event.getText().toString();
-			//getSystemService(Context.LOCATION_SERVICE)
+			// getSystemService(Context.LOCATION_SERVICE)
 			// And log them to LogCat
-			Log.v(TAG, "Message: "+ eventText + " at: "+ eventTime);
-			
+			Log.v(TAG, "Message: " + eventText + " at: " + eventTime);
+
 		}
 	}
 
 	@Override
 	public void onInterrupt() {
 		Log.v(TAG, "***** onInterrupt");
-		
+
 	}
-	
+
 	@Override
-    public void onServiceConnected()
-    {
-        Log.v(TAG, "***** onServiceConnected");
+	public void onServiceConnected() {
+		Log.v(TAG, "***** onServiceConnected");
 
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-        info.notificationTimeout = 100;
-        info.feedbackType = AccessibilityEvent.TYPES_ALL_MASK;
-        setServiceInfo(info);
+		AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+		info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+		info.notificationTimeout = 100;
+		info.feedbackType = AccessibilityEvent.TYPES_ALL_MASK;
+		setServiceInfo(info);
 
-    }
+	}
 
 }
