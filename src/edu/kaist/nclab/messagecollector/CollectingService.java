b5f3e908df5ecc2f79ff5357f6537b720e6840b3
Friedhelm <f.victor@kaist.ac.kr>
2014-05-15 14:15:44 +0200

    small refactoring to make it more readable

--- src/edu/kaist/nclab/messagecollector/CollectingService.java
+++ src/edu/kaist/nclab/messagecollector/CollectingService.java
@@ -14,8 +14,9 @@ import android.widget.Toast;
 
 public class CollectingService extends AccessibilityService {
 
+	// The tag used for logging
+	public static final String TAG = "CollectingService";
 
-	//ActivityManager actMng = (ActivityManager) this.getSystemService(Context.ACTIVITY_SERVICE);
 	@Override
 	public void onCreate() {
 		Log.v(TAG, "onCreate");
@@ -25,6 +26,60 @@ public class CollectingService extends AccessibilityService {
 
 	}
 
+	@Override
+	public void onAccessibilityEvent(AccessibilityEvent event) {
+		Log.v(TAG, "application name: "+getForegroundApplicationName());
+		Log.v(TAG, "***** onAccessibilityEvent with Text " + event.getText()
+				+ " package: " + event.getPackageName());
+		if(isKakaoTalkMessage(event)) {
+			// Then collect the following data
+			long messageTime = event.getEventTime();
+			String messageContent = event.getText().toString();
+			String foregroundApp = getForegroundApplicationName();
+			// And log them to LogCat
+			Log.v(TAG, "Message: " + messageContent + " at: " + messageTime + " while seeing "+foregroundApp);
+
+		}
+	}
+
+	@Override
+	public void onInterrupt() {
+		Log.v(TAG, "***** onInterrupt");
+
+	}
+
+	@Override
+	public void onServiceConnected() {
+		Log.v(TAG, "***** onServiceConnected");
+
+		AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+		info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
+		info.notificationTimeout = 100;
+		info.feedbackType = AccessibilityEvent.TYPES_ALL_MASK;
+		setServiceInfo(info);
+	}
+	
+	
+	/**
+	 * Checks whether the given event is a KakaoTalk Popup message
+	 * of message length greater than 2 (only those with content)
+	 * @param event the captured AccessibilityEvent
+	 */
+	private boolean isKakaoTalkMessage(AccessibilityEvent event) {
+		CharSequence charPackageName = event.getPackageName();
+		String eventPackageName;
+
+		if (charPackageName == null) {
+			eventPackageName = "ep";
+		} else {
+			eventPackageName = charPackageName.toString();
+		}
+		// IF it is a Kakao Talk Message Event of length > 2
+		return (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED
+				&& event.getText().toString().length() > 2
+				&& eventPackageName.contentEquals("com.kakao.talk"));
+	}
+	
 	/**
 	 * Returns the name of the application that is currently in the foreground.
 	 * If it is not possible, if that Applications Package name cannot be found,
@@ -50,54 +105,6 @@ public class CollectingService extends AccessibilityService {
 		}
 		
 		return foregroundTaskAppName;
-	}
-
-	public static final String TAG = "CollectingService";
-
-	@Override
-	public void onAccessibilityEvent(AccessibilityEvent event) {
-		Log.v(TAG, "application name: "+getForegroundApplicationName());
-		Log.v(TAG, "***** onAccessibilityEvent with Text " + event.getText()
-				+ " package: " + event.getPackageName());
-		CharSequence charPackageName = event.getPackageName();
-		String eventPackageName;
-
-		if (charPackageName == null) {
-			eventPackageName = "ep";
-		} else {
-			eventPackageName = charPackageName.toString();
-		}
-		// IF it is a Kakao Talk Message Event of length >= 3
-		if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED
-				&& event.getText().toString().length() >= 3
-				&& eventPackageName.contentEquals("com.kakao.talk")) {
-
-			// Then collect the following data
-			long eventTime = event.getEventTime();
-			String eventText = event.getText().toString();
-			// getSystemService(Context.LOCATION_SERVICE)
-			// And log them to LogCat
-			Log.v(TAG, "Message: " + eventText + " at: " + eventTime);
-
-		}
-	}
-
-	@Override
-	public void onInterrupt() {
-		Log.v(TAG, "***** onInterrupt");
-
-	}
-
-	@Override
-	public void onServiceConnected() {
-		Log.v(TAG, "***** onServiceConnected");
-
-		AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-		info.eventTypes = AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED;
-		info.notificationTimeout = 100;
-		info.feedbackType = AccessibilityEvent.TYPES_ALL_MASK;
-		setServiceInfo(info);
-
 	}
 
 }
